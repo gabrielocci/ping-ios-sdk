886a2532de49e560c6f8374a65cd17dc0184a8aa
Gabriele Paolocci <gabrielepaolocci+pingcorp@pingidentity.com>
2026-07-02 11:12:29 +0200

    feat: Add PingRecognize module scaffold

---
+++ Recognize/Recognize/Recognize.swift
@@ -0,0 +1,12 @@
+//
+//  Recognize.swift
+//  PingRecognize
+//
+//  Copyright (c) 2026 Ping Identity Corporation. All rights reserved.
+//
+//  This software may be modified and distributed under the terms
+//  of the MIT license. See the LICENSE file for details.
+//
+
+/// PingRecognize module placeholder. Replace this with your real implementation.
+public enum Recognize {}
---
+++ Recognize/RecognizeTests/RecognizeTests.swift
@@ -0,0 +1,19 @@
+//
+//  RecognizeTests.swift
+//  RecognizeTests
+//
+//  Copyright (c) 2026 Ping Identity Corporation. All rights reserved.
+//
+//  This software may be modified and distributed under the terms
+//  of the MIT license. See the LICENSE file for details.
+//
+
+import XCTest
+@testable import PingRecognize
+
+final class RecognizeTests: XCTestCase {
+    func testPlaceholder() throws {
+        // Replace this with real tests for PingRecognize.
+        XCTAssertTrue(true)
+    }
+}
